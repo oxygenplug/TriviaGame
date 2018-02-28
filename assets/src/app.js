ccd061ebd6e7cb9fbb2869097ec6ef0f9881968a
oxygenplug <dan.garces2@gmail.com>
2018-02-28 13:08:42 -0500

    added logic to see if user selected right or wrong answer

--- assets/src/app.js
+++ assets/src/app.js
@@ -2,32 +2,40 @@ $(document).ready(function () {
   //document.getElementById('timer').innerHTML = setInterval(() => this.clock = Date.now(), 1000);
 
   var timeLeft = 59;
-
   const runTimer = function () {
-    if (timeLeft > 0) {
+    if (timeLeft >= 0) {
 
-    
-    displayTime();
-    timeLeft = timeLeft - 1;
-    if (timeLeft === 0) {
-      return;
+
+      displayTime();
+      timeLeft = timeLeft - 1;
+      if (timeLeft === 0) {
+        return;
+      }
     }
-  }
   };
 
   var displayTime = function () {
-    
+
     document.getElementById('timer').innerHTML = timeLeft;
 
   };
   setInterval(runTimer, 1000);
 
-
-  var triviaGame = {
-
-    totalScore: 0,
-    currentScore: 0,
-
+  var totalScore = 0;
+  var currentScore = 0;
+  var totalWrong = 0;
+  var selectCorrectAnswer = function () {
+    currentScore = currentScore + 1;
+    console.log("The total correct is currently " + currentScore);
   };
+
+  var selectWrongAnswer = function () {
+    totalWrong = totalWrong + 1;
+    console.log("The total wrong is currently " + totalWrong);
+  };
+
+
+  $('.correct').on('click', selectCorrectAnswer);
+  $('.wrong').on('click', selectWrongAnswer);
 
 });
